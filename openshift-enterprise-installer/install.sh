0fc54bf34700a7808c69fa3fbd6af7bd3417026e
root <root@ose3lab.click2cloud.net>
2016-04-17 15:18:57 +0530

    scipt

--- openshift-enterprise-installer/install.sh
+++ openshift-enterprise-installer/install.sh
@@ -1,3 +1,19 @@
+#region Copyright ©2016, Click2Cloud Inc. - All Rights Reserved
+#* -------------------------------------------------------------------- *
+#*                            Click2Cloud Inc.                          *
+#*                  Copyright ©2016 - All Rights reserved               *
+#*                                                                      *
+#*                                                                      *
+#*  Copyright © 2016 by Click2Cloud Inc. | www.click2cloud.net          *
+#*  All rights reserved. No part of this publication may be reproduced, *
+#*  stored in a retrieval system or transmitted, in any form or by any  *
+#*  means, photocopying, recording or otherwise, without prior written  *
+#*  consent of Click2cloud Inc.                                         *
+#*                                                                      *
+#*                                                                      *
+#* -------------------------------------------------------------------- *
+#endregion Copyright ©2016, Click2Cloud Inc. - All Rights Reserved
+#
 chk_subscription()
 {
 
